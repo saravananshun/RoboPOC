1b00321dac06023c42875bdf1e1a79b7b53dc833
saravanan shanmugavel <saravanan.shun@gmail.com>
2018-09-09 18:47:57 -0400

    init

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+declare var jquery: any;
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  model: any = {};
+
+  public ngOnInit() {
+
+  }
+
+  public onSubmit() {
+    if ($("#username").val() == null || $("#username").val() == "") {
+      $('#emptyUidError').toggleClass("hidden shown");
+    }
+
+    if ($("#password").val() == null || $("#password").val() == "") {
+      $('#emptyPasswordError').toggleClass("hidden shown");
+    }
+
+  }
+
 }
+
