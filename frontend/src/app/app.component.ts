f937dc2e3ead1b5336f147908bcd96b4c6757523
saravanan shanmugavel <saravanan.shun@gmail.com>
2018-09-16 17:14:58 -0400

    initial

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {MatDialog} from "@angular/material";
+import {ConfirmationdialogComponent} from "./confirmationdialog/confirmationdialog.component";
 
 declare var jquery: any;
 declare var $: any;
@@ -10,23 +12,12 @@ declare var $: any;
 })
 export class AppComponent implements OnInit {
   title = 'app';
-  model: any = {};
 
   public ngOnInit() {
 
   }
 
-  public onSubmit() {
-    /*if ($("#username").val() == null || $("#username").val() == "") {
-      $('#emptyUidError').toggleClass("hidden shown");
-    }
-
-    if ($("#password").val() == null || $("#password").val() == "") {
-      $('#emptyPasswordError').toggleClass("hidden shown");
-    }*/
-
-    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.model))
-
+  constructor() {
   }
 
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import {MatDialogModule, MatButtonModule} from '@angular/material';
+
 
 import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
+import { ConfirmationdialogComponent } from './confirmationdialog/confirmationdialog.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConfirmationdialogComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    MatDialogModule,
+    MatButtonModule,
+    BrowserAnimationsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [ ConfirmationdialogComponent ]
 })
 export class AppModule { }
